981196640b4f150c28252a4346114249f5664496
Joseph Falzarano <jfalzarano888@gmail.com>
2017-11-16 13:10:15 -0600

    boo

--- app.js
+++ app.js
@@ -15,33 +15,44 @@ let matchCards = [];
 let displayRow1 = [];
 let displayRow2 = [];
 let displayRow3 = ["butthole", "fuck", "ass"];
-// const $modal = $("#match-row1");
-// const $modalClose = $modal.children("name");
-//
-//
-// console.log($modalClose);
+const $modal = $("#match-row1");
 
+const $test = $modal.find( 'div[name="card0"]' );
+//
+// const test2 = displayRow2.find("div[name='card0']" )
+//
+// console.log($test2);
 
 $(window).load(function() {
     console.log('hello');
     displayGameCards(imgRoot);
-
 });
 
 $(document).on("click", ".card-frame", function(){
-  if ( $(".selected").length >= 2 && displayRow1[0] == displayRow2[0]){
-    console.log("match!");
-    return;
-  } else if ( $(".selected").length >= 2 && displayRow1[1] == displayRow2[1]){
-    console.log("match!");
-    return;
-  } else if ($(".selected").length >= 2 && displayRow1[2] == displayRow2[2]){
-    console.log("match!");
-    return;
-  }
+
+  console.log($name);
+  var $name = $(this).find('div[name="card0"]');
+
+  //console.log(displayRow2[0]);
+  // if (displayRow1[0] == displayRow2[0] && $(".selected")) {
+  //   return alert("you finally did it");
+  // }
+
+  // for(var i in displayRow1) {
+  //   for (var j in displayRow2) {
+  //     if(displayRow1[i] == displayRow2[j]) {
+  //       return alert("its a match!");
+  //     }
+  //   }
+  // }
+  // if ( $(".selected").length >= 2 {
+  //   return;
+  // }
+
   $(this).find(".card-front").removeClass("hidden");
   $(this).find(".card-back").addClass("hidden");
   $(this).addClass("selected");
+  //console.log(displayRow2[1]);
 })
 
 function getRandomInt(array) {
@@ -83,14 +94,17 @@ function displayGameCards(url) {
               <img class="card-back" src="pictures/face.svg" alt="card-back">
           </div>`);
     });
-    console.log(displayRow2);
+    //console.log(displayRow2[0]);
     shuffle(displayRow2);
-    console.log(displayRow2[0]);
+    //console.log(displayRow2[0]);
     $("#match-row1").append(displayRow1);
     $("#match-row2").append(displayRow2);
+    const $modal = $("#match-row1");
+    const $test = $modal.find( "div[name='card0']" );
+    //console.log($test);
   });
 }
 
 // console.log(displayRow3.length);
-// console.log(displayRow2.length);
+//console.log(displayRow2[0]);
 // shuffle(displayRow1);
